b817cfe86cce37dba81ae883a88ddb998e647242
alhymov <alhymov@mail.ru>
2016-04-01 03:00:27 +0300

    Create

---
+++ uRESTDebuggerResStrs.pas
@@ -0,0 +1,31 @@
+unit uRESTDebuggerResStrs;
+
+interface
+
+resourcestring
+  RSAddCustomParameter = 'Add Custom Parameter';
+  RSAddParameter = 'Add Parameter';
+  RSBytesOfDataReturnedAndTiming = '%d : %s - %d bytes of data returned. Timing: Pre: %dms - Exec: %dms - Post: %dms - Total: %dms';
+  RSCannotProceedWithoutRequest = 'Cannot proceed without request.';
+  RSComponentsCopied = 'The following components have been copied to the clipboard: %s';
+  RSConfirmClearRecentRequests = 'Are you sure that you want to clear list of the most recent requests?';
+  RSConfirmDeleteCustomParameter = 'Are you sure you want to delete the custom parameter?';
+  RSContentIsNotJSON = 'Content is not JSON';
+  RSContentIsValidJSON = 'Content is valid JSON';
+  RSEditCustomParameter = 'Edit Custom Parameter';
+  RSEditParameter = 'Edit Parameter';
+  RSInvalidRootElement = 'Invalid root element';
+  RSNoCustomParameterSelected = 'No custom parameter selected.';
+  RSProvideAuthCode = 'Authenication Code';
+  RSProvideAuthEndPoint = 'Authenication End Point';
+  RSProvideClientID = 'Client Id';
+  RSProvideClientIDAndClientSecret = 'Client Id and Client Secret';
+  RSProvideTokenEndPoint = 'Token End Point';
+  RSProxyServerDisabled = 'Proxy-server disabled';
+  RSProxyServerEnabled  = 'Proxy-server enabled: ';
+  RSRootElementAppliesToJSON = 'Root Element, "%s" not applied.  Root Element is only applied to JSON content.  Content is not JSON.';
+  RSUnableToValidateCertifcate = 'Unable to validate certificate.';
+
+implementation
+
+end.
